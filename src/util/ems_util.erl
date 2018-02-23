23a0e43c5d11814a9105e40d5f05d640bf79834d
Everton Agilar <evertonagilar@gmail.com>
2018-02-23 14:50:51 -0300

    improve netadm

--- src/util/ems_util.erl
+++ src/util/ems_util.erl
@@ -169,10 +169,10 @@ version() ->
 
 -spec server_name() -> string().
 server_name() ->
-	io_lib:format(<<"ems-bus-~s">>, [case application:get_key(ems_bus, vsn) of 
-											{ok, Version} -> Version;
-											undefined -> "1.0.0"
-									 end]).
+	iolist_to_binary([<<"ems-bus-">>, [case application:get_key(ems_bus, vsn) of 
+											{ok, Version} -> list_to_binary(Version);
+											undefined -> <<"1.0.0">>
+									  end]]).
 
 %% Retorna o hash da url e os parâmetros do request
 hashsym_and_params(S) when is_binary(S) -> hashsym_and_params(binary_to_list(S), 1, 0, []);
