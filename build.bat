2f1a911fe1798cd39a6f360f751ef380bc48b17d
evertonagilar <evertonagilar@gmail.com>
2018-10-26 09:37:20 -0300

    build windows

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 @echo off
 cls
+echo Copiando dependencia jiffy para deps...
+xcopy /e /q /y deps_win\jiffy\* deps\jiffy
 echo Build ems-bus with rebar...
 if not exist priv\log goto DeleteDBFolder
 rmdir /S /Q priv\log
@@ -8,3 +10,5 @@ if not exist priv\db goto BuildWithRebar
 rmdir /S /Q priv\db
 :BuildWithRebar
 tools\rebar\rebar clean get-deps compile --config rebar_win.config
+
+
--- start.bat
+++ start.bat
@@ -3,7 +3,7 @@ REM
 REM author Everton de Vargas Agilar <<evertonagilar@gmail.com>>
 REm
 erl -pa .\ebin deps\rfc3339\ebin deps\jiffy\ebin deps\jesse\ebin deps\jsx\ebin deps\poolboy\ebin deps\cowboy\ebin deps\cowlib\ebin deps\json_rec\ebin deps\mochiweb\ebin deps\oauth2\ebin deps\parse_trans\ebin deps\ranch\ebin deps\esqlite\ebin deps\soap\ebin -sname emsbus -setcookie erlangms -eval "ems_bus:start()" -boot start_sasl -config .\priv\conf\elog
-xcopy deps_win\jiffy deps
+
 
 
 		
